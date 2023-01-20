905686145e93c051c741768424711ee29e0023ad
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-20 20:37:05 +0300

    Изменения в файле example 002

--- Examples/example2/Example 002.cs
+++ Examples/example2/Example 002.cs
@@ -3,28 +3,17 @@
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 99 & number < 1000)
+while (number > 999)
 {
-    Console.WriteLine($"Третья цифра числа {number} равна {(number / 10) % 10}");
+    number /= 10;
+}
+
+if (number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
 }
 
 else
 {
-    if (number > 1000 & number < 10000)
-    {
-        Console.WriteLine($"Третья цифра числа {number} равна {(number % 100) / 10 }");
-    }
-
-    else
-    {
-        if (number > 10000 & number < 100000)
-        {
-            Console.WriteLine($"Третья цифра числа {number} равна {(number % 1000) / 100}");
-        }
-
-        else
-        {
-            Console.WriteLine($"У числа {number} третьей цифры нет");
-        }
-    }
+    Console.WriteLine(number % 10);
 }
