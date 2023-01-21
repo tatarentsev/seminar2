3f6a9f55ad56b6718848d678d77af603b04720ac
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-21 13:15:52 +0300

    изменения в файле example 003

--- Examples/example3/example 003.cs
+++ Examples/example3/example 003.cs
@@ -1,14 +1,15 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. 6,7,1
 
 Console.Write("Введите цифру, обозначающую день недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-if (day > 5)
+string DayOfWeek(int arg1)
 {
-    Console.WriteLine("да");
+    string result = "";
+    if (arg1 > 0 & arg1 <= 5) result = "нет";
+    if (arg1 > 5 & arg1 < 8) result = "да";
+    return result;
 }
 
-else
-{
-    Console.WriteLine("нет");
-}
+string day = DayOfWeek(number);
+Console.Write(day);
