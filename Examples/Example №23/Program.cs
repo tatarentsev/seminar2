70f345a25b34cb2ce1128afd4cf626ad72d28174
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-22 13:03:34 +0300

    Example №23

---
+++ Examples/Example №23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+Console.Write($"{Math.Pow(i, 3)},");
+}
+Console.WriteLine("\b ");
