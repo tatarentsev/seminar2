0dd35770fd20b53fe5db4ba5539fcc1aa96578c3
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-01-22 11:55:12 +0300

    Second Example

---
+++ Examples/№19/Example19.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число, а я проверю является ли оно палиндромом: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] & number[1] == number[3]) 
+    {
+        Console.Write("да");
+    }
+
+    else
+    {
+        Console.Write("нет");
+    }
+}
---
+++ Examples/№23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)},");
+}
+Console.WriteLine("\b ");
